340ea2ae9fa57ed419030a96911c8632b656caeb
richi <richi@172.20.7.60>
2017-02-21 10:39:06 -0600

    Se Agregaron las clases de los tipos de lista

---
+++ src/CList.java
@@ -0,0 +1,16 @@
+
+public class CList implements ILista {
+
+	@Override
+	public void add(int index, Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/ILista.java
+++ src/ILista.java
@@ -1,5 +1,5 @@
 
-public interface Lista<E> {
+public interface ILista<E> {
 	
 	public void add(int index, E obj); //Agrega un objeto a la lista
 	public E getFirst(); //Obtiene un objeto de la lista
